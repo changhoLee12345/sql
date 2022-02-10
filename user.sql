b887826781d72a2ec2cd70f0e17782a02749db82
chaneho <changho@email.com>
2022-02-10 15:11:07 +0900

    'b'

--- user.sql
+++ user.sql
@@ -6,5 +6,8 @@ role     varchar2(5));
 
 insert into users values('test','test123','包府磊','Admin');
 insert into users values('user1','user1','全辨悼','User');
+insert into users values('test','test123','包府磊','Admin');
+insert into users values('user1','user1','全辨悼','User');
+
 
 commit;
