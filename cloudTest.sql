60890ba8b0784cc26cfa5f90d6cb76da62856632
newleadon <newleadon@email.com>
2022-11-23 00:27:21 +0900

    mod

--- cloudTest.sql
+++ cloudTest.sql
@@ -4,7 +4,7 @@ select * from member;
 
 alter table member rename column mail to email;
 
-select * from tbl_board;
+select * from tbl_board order by 1 desc;
 
 drop table tbl_reply purge;
 
@@ -16,3 +16,21 @@ create table tbl_reply (
  writer varchar2(100) not null,
  create_date date default sysdate
 );
+
+select * from tbl_reply;
+
+create table BOOK_INFO (
+ book_code varchar2(20) primary key,
+ book_title varchar2(30),
+ book_author varchar2(50),
+ book_press varchar2(50),
+ book_price number
+);
+
+insert into book_info values('B0001', '이것이자바다', '김자바','자바출판사',1000);
+insert into book_info values('C0001', '이것이씨다', '김씨','매우출판사',2000);
+
+select * from book_info;
+
+
+
