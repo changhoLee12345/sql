787dcd82d15828bdf29589c246300fe8e5f4770e
hansung <hansung@email>
2022-12-24 20:55:12 +0900

    M

--- cloudTest.sql
+++ cloudTest.sql
@@ -7,7 +7,6 @@ alter table member rename column mail to email;
 select * from tbl_board order by 1 ;
 
 drop table tbl_reply purge;
-
 create sequence reply_seq;
 create table tbl_reply (
  reply_no int primary key,
@@ -36,8 +35,6 @@ select * from book_info;
 select * from book_info
 where 1 = 1
 and book_title = '이것이자바다' or book_author = '김씨';
-
-
 
 delete from book_info where book_code like 'Groucho%';
 
@@ -74,7 +71,28 @@ create table tbl_notice (
  attach_dir varchar2(100)
 );
 
+select notice_seq.nextval from dual;
 insert into tbl_notice (notice_id, notice_writer, notice_title, notice_subject, notice_date)
 values(1, 'user1', 'sample title', 'sample subject', sysdate);
 
-select * from tbl_notice;
+insert into tbl_notice (notice_id, notice_writer, notice_title, notice_subject, notice_date)
+select notice_seq.nextval, 'writer'||notice_seq.currval, 'title'||notice_seq.currval, 'subject'||notice_seq.currval, sysdate
+from tbl_notice;
+
+select b.notice_id, b.notice_writer, b.notice_title, b.notice_date, b.hit_count from
+(
+    select rownum rn, a.* from 
+    (select n.* 
+     from tbl_notice n
+     where 1 = 1
+     --and notice_writer like '%writer3%'
+     --and notice_title like '%title3%'
+     --and notice_subject like '%subject31%'
+     order by 1) a
+    where rownum <= (1*10)
+) b
+where b.rn >= (1*10-9);
+
+select * from tbl_notice
+where notice_id = 1;
+
