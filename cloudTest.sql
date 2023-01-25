1d074f7f98ca661a409ae1d26f1a386c1b6e9e62
hansung <hansung@email>
2023-01-25 22:08:15 +0900

    aa

--- cloudTest.sql
+++ cloudTest.sql
@@ -1,90 +1,16 @@
 select * from tab;
 
-select * from members;
 
-alter table member rename column mail to email;
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+insert into tbl_board (board_no, title, content, writer)
+values(board_seq.nextval, 'title'||board_seq.currval, 'content'||board_seq.currval, 'writer'||board_seq.currval);
 
-select * from tbl_board order by 1 ;
+select board_seq.currval from dual;
 
-drop table tbl_reply purge;
-
-create sequence reply_seq;
-create table tbl_reply (
- reply_no int primary key,
- bno int not null,
- content varchar2(300) not null,
- writer varchar2(100) not null,
- create_date date default sysdate
-);
-
-select * from tbl_reply;
-
-create table BOOK_INFO (
- book_code varchar2(20) primary key,
- book_title varchar2(30),
- book_author varchar2(50),
- book_press varchar2(50),
- book_price number
-);
-
-select * from book_info order by book_code;
-delete from book_info where book_code like 'Groucho%';
-
-create table book_file (
- file_no number primary key,
- book_code varchar2(50),
- book_image varchar2(50),
- book_path varchar2(50)
-);
-select * from book_file;
-delete from book_file where book_code like 'Groucho%';
-
--- members table.
-create table members (
- id varchar(10) primary key,
- passwd varchar2(10),
- name varchar2(30),
- email varchar2(30),
- responsibility varchar2(10),
- pfileName varchar2(50),
- ofileName varchar2(50),
- cdate date
-);
-insert into members values('user1','1234','username','email.com','user',null,null,sysdate);
-
-drop table genesis purge;
-create table genesis (
- member_order varchar2(3) primary key,
- member_name varchar2(100),
- col1 char(1) default 'N',
- col2 char(1) default 'N',
- col3 char(1) default 'N',
- col4 char(1) default 'N',
- col5 char(1) default 'N',
- col6 char(1) default 'N',
- col7 char(1) default 'N',
- col8 char(1) default 'N',
- col9 char(1) default 'N',
- col10 char(1) default 'N',
- col11 char(1) default 'N',
- col12 char(1) default 'N',
- col13 char(1) default 'N',
- col14 char(1) default 'N',
- col15 char(1) default 'N'
-);
-
-select * from genesis
-order by member_order;
-
-update genesis   set col1 = 'Y'   where member_order = '1-1';
-update genesis set col2 = 'Y' where member_order = '11';
-=======
-select * from members order by 1;
-alter table member rename column mail to email;
-alter table members add (phone_number varchar2(20));
-alter table members add (addr varchar2(100));
-
-select * from tbl_board order by 1 ;
+select * from tbl_board
+order by 1 desc;
+delete from tbl_board where board_no > 500;
 
 drop table tbl_reply purge;
 create sequence reply_seq;
@@ -95,9 +21,13 @@ create table tbl_reply (
  writer varchar2(100) not null,
  create_date date default sysdate
 );
-
 select * from tbl_reply;
 
+
+
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
 -- book_info table.
 create table BOOK_INFO (
  book_code varchar2(20) primary key,
@@ -130,6 +60,34 @@ create table book_file (
 select * from book_file;
 delete from book_file where book_code like 'Groucho%';
 
+
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+-- members table.
+create table members (
+ id varchar(10) primary key,
+ passwd varchar2(10),
+ name varchar2(30),
+ email varchar2(30),
+ responsibility varchar2(10),
+ pfileName varchar2(50),
+ ofileName varchar2(50),
+ cdate date
+);
+insert into members values('user1','1234','username','email.com','user',null,null,sysdate);
+
+=======
+select * from members order by 1;
+alter table member rename column mail to email;
+alter table members add (phone_number varchar2(20));
+alter table members add (addr varchar2(100));
+
+select * from tbl_board order by 1 ;
+
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
 -- tbl_notice table.
 create sequence notice_seq;
 create table tbl_notice (
@@ -177,6 +135,10 @@ set notice_writer = case mod(notice_id , 4) when 1 then 'user1'
 where notice_id > 10;
 select * from tbl_notice order by 1;
 
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+
 -- center_info table.
 drop table center_info purge;
 create table center_info (
@@ -202,5 +164,35 @@ select * from center_info order by 1;
 insert into center_info (id, center_name, center_type, facility_name, org, phone_number, sido, sigungu, address, lat, lng, zip_code, created_at, updated_at)
 values(1,'centername','centertype','facilityname','org','phone-number','sido','sigungu','address','lat','lng','zipcode','createdat','updatedat');
 
-select * from tab;
-select * from tab;
+
+
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+
+drop table genesis purge;
+create table genesis (
+ member_order varchar2(3) primary key,
+ member_name varchar2(100),
+ col1 char(1) default 'N',
+ col2 char(1) default 'N',
+ col3 char(1) default 'N',
+ col4 char(1) default 'N',
+ col5 char(1) default 'N',
+ col6 char(1) default 'N',
+ col7 char(1) default 'N',
+ col8 char(1) default 'N',
+ col9 char(1) default 'N',
+ col10 char(1) default 'N',
+ col11 char(1) default 'N',
+ col12 char(1) default 'N',
+ col13 char(1) default 'N',
+ col14 char(1) default 'N',
+ col15 char(1) default 'N'
+);
+
+select * from genesis
+order by member_order;
+
+update genesis   set col1 = 'Y'   where member_order = '1-1';
+update genesis set col2 = 'Y' where member_order = '11';
