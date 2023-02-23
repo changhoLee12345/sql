cec68655b76398036760c204d8663958c0025623
leadon <leadon@pc.com>
2023-02-23 20:17:14 +0900

    a

--- docker_dev.sql
+++ docker_dev.sql
@@ -17,54 +17,6 @@ insert into product values('P2023004', '에티오피아 예가체프','에티오
 insert into product values('P2023005', '케냐 오크라톡신','케냐 오크라톡신 맛있는 커피입니다', 4500, 3000, 3.0, '케냐 오크라톡신.jpg');
 insert into product values('P2023006', '코스타리카 따라주','코스타리카 따라주 맛있는 커피입니다', 3000, 2500, 5.0, '코스타리카 따라주.jpg');
 
-select * from (
-  select * from product
-  order by like_it desc
-) where rownum <= 4;
-
-
-update product
-set like_it = 5
-where product_code = 'P2023006';
-
-create table tbl_sample1(col1 varchar2(500));
-drop table tbl_sample2 purge;
-create table tbl_sample2(col2 varchar2(50));
-
-select * from tbl_reply
-order by 1 desc;
-
-create index idx_reply on tbl_reply (bno desc, rno asc);
-
-select rno, bno, reply, replyer, replydate, updatedate
-from (
-    select /*+INDEX(tbl_reply idx_reply) */
-    rownum rn, bno, rno, reply, replyer, replyDate, updatedate
-    from tbl_reply
-    where bno = 3
-    and rno > 0
-    and rownum <= 10
-) where rn >= 1;
-
-select * from (
-  select /*+INDEX_DESC(tbl_board pk_board) */
-         rownum rn, bno, title, content, writer, regdate, updatedate
-  from tbl_board
-  where (title like '%test%' or content like '%test%')
-  and   rownum <= 20
-)
-where rn > 10;
-
-update tbl_board
-set writer = case mod(bno, 4) when 0 then 'user00' 
-                              when 1 then 'user01'
-                              when 2 then 'user02'
-                              when 3 then 'user03'
-             end;
-where bno > 0;
-
-select count(*) from tbl_board 
-where bno > 0;
 
 -----------------------------------------------
 create sequence seq_board;
@@ -76,6 +28,7 @@ create table tbl_board (
  regdate date default sysdate,
  updatedate date default sysdate);
 alter table tbl_board add constraint pk_board primary key(bno);
+alter table tbl_board add (replycnt number default 0);
 -----------------------------------------------
 create sequence seq_reply;
 create table tbl_reply (
@@ -88,22 +41,15 @@ create table tbl_reply (
 );
 alter table tbl_reply add constraint pk_reply primary key(rno);
 alter table tbl_reply add constraint fk_reply_board foreign key (bno) references tbl_board (bno);
+create index idx_reply on tbl_reply (bno desc, rno asc);
 -----------------------------------------------
 
-insert into tbl_board (bno, title, content, writer)
-values(seq_board.nextval, 'test title', 'test content', 'user01');
-
-
-insert into tbl_board (bno, title, content, writer)
-select seq_board.nextval, title, content, writer 
-from tbl_board;
-
-select * from tbl_board 
-order by 1 desc;
-
-select to_char(replydate,'rrrr/mm/dd hh24:mi:ss') from tbl_reply order by 1 desc;
-select sysdate,to_char(sysdate,'rrrr/mm/dd hh24:mi:ss')  from dual;
-
-select * from tbl_reply;
-insert into tbl_reply (rno, bno, reply, replyer)
-values(seq_reply.nextval, 3, 'reply'||seq_reply.currval, 'replyer2');
+create table tbl_attach (
+  uuid varchar2(100) not null,
+  uploadPath varchar2(200) not null,
+  fileName varchar2(100) not null,
+  fileType char(1) default 'I',
+  bno number(10, 0)
+);
+alter table tbl_attach add constraint pk_attach primary key (uuid);
+alter table tbl_attach add constraint fk_board_attach foreign key (bno) references tbl_board(bno);
