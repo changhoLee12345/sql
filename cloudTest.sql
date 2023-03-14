3c1fa1633596715bd6732cbfb36f2d7138f7b95c
leadon <leadon@pc.com>
2023-03-14 22:20:47 +0900

    aa

--- cloudTest.sql
+++ cloudTest.sql
@@ -99,12 +99,18 @@ create table members (
  ofileName varchar2(50),
  cdate date
 );
-insert into members values('user1','1234','username','email.com','user',null,null,sysdate);
 
-select * from members order by 1;
 alter table member rename column mail to email;
 alter table members add (phone_number varchar2(20));
 alter table members add (addr varchar2(100));
+
+insert into members values('user6','1234','사용자6','user6@email','User','member6.png',null,sysdate,'010-1111-2222','Daegu, jungangno-6');
+
+select * from members order by 1;
+delete from members where responsibility='User';
+update members
+set pfilename = 'admin1.png'
+where id='admin';
 
 select * from tbl_board order by 1 ;
 
