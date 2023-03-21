c3030c462c3a563f14c60057c09b169b8995bc93
linuxie <linuxie@mail.com>
2023-03-21 19:34:49 +0900

    mo

--- cloudDev.sql
+++ cloudDev.sql
@@ -1,1 +1,8 @@
 select * from tab;
+
+select * from book_info
+order by 1;
+
+select * from tbl_board;
+select board_seq.nextval from dual;
+insert into tbl_board values(board_seq.nextval, 'test', 'test', 'user01', to_date('2022-01-01','yyyy-mm-dd'), 0, null);
