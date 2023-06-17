fe6839a5e3185bd677a499181094c0c53e888e5f
linuxie <linuxie@email.com>
2023-06-17 15:44:42 +0900

    first

--- cloudAdmin.sql
+++ cloudAdmin.sql
@@ -1,3 +1,9 @@
+
 select * from all_users order by user_id;
 
 alter user dev identified by '';
+
+alter session set "_ORACLE_SCRIPT" = true;
+
+create user proj identified by proj;
+grant connect, resource to proj;
