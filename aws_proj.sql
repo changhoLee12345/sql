90440f3b38945edef508fc73a9532eed814439e5
linuxie <linuxie@email.com>
2023-06-23 21:44:49 +0900

    modify

---
+++ aws_proj.sql
@@ -0,0 +1,157 @@
+select * from tab;
+
+select * from tab;
+
+drop table member purge;
+create table member (
+ id varchar2(10) primary key,
+ name varchar2(50) not null,
+ passwd varchar2(10) not null,
+ email varchar2(100)
+);
+insert into member(id, name, passwd, email) values('user1','사용자1','1111','user1@email.com');
+insert into member(id, name, passwd, email) values('user2','사용자2','1111','user2@email.com');
+insert into member(id, name, passwd, email) values('user3','사용자3','1111','user3@email.com');
+
+select * from member;
+
+select * from tbl_board order by bno desc;
+select * from tbl_attach;
+
+create table tbl_sample1(col1 varchar2(500));
+drop table tbl_sample2 purge;
+create table tbl_sample2(col2 varchar2(50));
+
+select * from tbl_reply
+order by 1 desc;
+
+select * from tbl_reply where bno = 2490387
+order by 1;
+
+insert into tbl_reply (rno, bno, reply, replyer)
+select seq_reply.nextval, bno, reply, replyer from tbl_reply where bno = 2490387;
+
+
+select rno, bno, reply, replyer, replydate, updatedate
+from (
+    select /*+INDEX(tbl_reply idx_reply) */
+    rownum rn, bno, rno, reply, replyer, replyDate, updatedate
+    from tbl_reply
+    where bno = 3
+    and rno > 0
+    and rownum <= 10
+) where rn >= 1;
+
+select /*+ INDEX_DESC(tbl_board pk_board) */
+*
+from tbl_board
+where bno > 0;
+
+select /*+ INDEX_ASC(tbl_board pk_board) */ *
+from tbl_board 
+where bno > 0;
+
+select rn, bno, title, content, writer, regdate, updatedate
+from (
+    select /*+ INDEX_DESC(tbl_board pk_board) */ rownum rn, bno, title, content, writer, regdate, updatedate
+    from tbl_board
+    where rownum <= 20 
+)
+where rn > 10
+--order by bno
+;
+
+select * from (
+  select /*+INDEX_DESC(tbl_board pk_board) */
+         rownum rn, bno, title, content, writer, regdate, updatedate
+  from tbl_board
+  where (title like '%test%' or content like '%test%')
+  and   rownum <= 20
+)
+where rn > 10;
+
+update tbl_board
+set writer = case mod(bno, 4) when 0 then 'user00' 
+                              when 1 then 'user01'
+                              when 2 then 'user02'
+                              when 3 then 'user03'
+             end
+where bno > 0;
+
+select count(*) from tbl_board 
+where bno > 0;
+
+insert into tbl_board (bno, title, content, writer)
+select seq_board.nextval, title, content, writer 
+from tbl_board;
+
+select * from tbl_board 
+order by bno desc;
+
+select to_char(replydate,'rrrr/mm/dd hh24:mi:ss') from tbl_reply order by 1 desc;
+select sysdate,to_char(sysdate,'rrrr/mm/dd hh24:mi:ss')  from dual;
+
+insert into tbl_reply (rno, bno, reply, replyer)
+values(seq_reply.nextval, 2490385, 'reply'||seq_reply.currval, 'replyer2');
+
+create table tbl_attach (
+  uuid varchar2(100) not null,
+  uploadPath varchar2(200) not null,
+  fileName varchar2(100) not null,
+  fileType char(1) default 'I',
+  bno number(10,0)
+);
+alter table tbl_attach add constraint pk_attach primary key (uuid);
+alter table tbl_attach add constraint fk_board_attach foreign key (bno) references tbl_board(bno);
+
+-----------------------------------------------
+-----------------------------------------------
+create sequence seq_board;
+create table tbl_board (
+ bno number(10,0),
+ title varchar2(200) not null,
+ content varchar2(2000) not null,
+ writer varchar2(50) not null,
+ regdate date default sysdate,
+ updatedate date default sysdate);
+alter table tbl_board add constraint pk_board primary key(bno);
+
+insert into tbl_board (bno, title, content, writer)
+values(seq_board.nextval, 'test title', 'test content', 'user01');
+
+select * from tbl_board order by 1 desc;
+
+-----------------------------------------------
+-----------------------------------------------
+create sequence seq_reply;
+create table tbl_reply (
+  rno number(10,0),
+  bno number(10,0) not null,
+  reply varchar2(1000) not null,
+  replyer varchar2(50) not null,
+  replydate date default sysdate,
+  updatedate date default sysdate
+);
+alter table tbl_reply add constraint pk_reply primary key(rno);
+alter table tbl_reply add constraint fk_reply_board foreign key (bno) references tbl_board (bno);
+create index idx_reply on tbl_reply (bno desc, rno asc);
+-----------------------------------------------
+-----------------------------------------------
+
+
+--T20230213(3강의장)
+create table product (
+ product_code varchar2(10) primary key,
+ product_name varchar2(100) not null, --상품명
+ product_desc varchar2(1000) not null,
+ product_price number not null,
+ sale_price number,
+ like_it number(3,1),
+ image varchar2(100)
+);
+insert into product values('P2023001', '과테말라 안티구아','과테말라 안티구아 맛있는 커피입니다', 5000, 4500, 4.5, '과테말라 안티구아.jpg');
+insert into product values('P2023002', '니카라구아 아라비카','니카라구아 아라비카 맛있는 커피입니다', 5500, 4500, 4.0, '니카라구아 아라비카.jpg');
+insert into product values('P2023003', '브라질산토스','브라질산토스 맛있는 커피입니다', 6000, 5000, 3.5, '브라질산토스.jpg');
+insert into product values('P2023004', '에티오피아 예가체프','에티오피아 예가체프 맛있는 커피입니다', 4000, 3500, 4.0, '에티오피아 예가체프.jpg');
+insert into product values('P2023005', '케냐 오크라톡신','케냐 오크라톡신 맛있는 커피입니다', 4500, 3000, 3.0, '케냐 오크라톡신.jpg');
+insert into product values('P2023006', '코스타리카 따라주','코스타리카 따라주 맛있는 커피입니다', 3000, 2500, 5.0, '코스타리카 따라주.jpg');
---
+++ oci_test.sql
@@ -0,0 +1,247 @@
+select * from tab
+order by tname;
+
+--git form연습.
+select /*+ INDEX_DESC(tbl_board SYS_C0031805) */
+*
+from tbl_board
+where board_no > 0;
+
+update tbl_board
+set writer = case mod(board_no, 4) when 0 then 'user0'
+                                when 1 then 'user1'
+                                when 2 then 'user2'
+                                else 'user3'
+            end
+where board_no > 0;
+
+select /*+ FULL(tbl_board) */
+rownum rn, board_no, title
+from tbl_board
+where board_no > 0
+order by board_no;
+
+select rn, board_no, title, content
+from (
+    select /*+ INDEX_DESC(tbl_board SYS_C0031805) */
+    rownum rn, board_no, title, content
+    from tbl_board
+    where rownum <= 20
+) where rn > 10;
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+--여러건 넣기.
+insert into tbl_board (board_no, title, content, writer)
+select board_seq.nextval, title, content, writer from tbl_board;
+select count(*) from tbl_board;
+
+insert into tbl_board (board_no, title, content, writer)
+values(board_seq.nextval, 'title'||board_seq.currval, 'content'||board_seq.currval, 'writer'||board_seq.currval);
+
+select * from tbl_board order by 1 desc;
+
+delete from tbl_board where board_no > 500;
+
+drop table tbl_reply purge;
+create sequence reply_seq;
+create table tbl_reply (
+ reply_no int primary key,
+ bno int not null,
+ content varchar2(300) not null,
+ writer varchar2(100) not null,
+ create_date date default sysdate
+);
+select * from tbl_reply;
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+-- book_info table.
+drop table book_info purge;
+create table BOOK_INFO (
+ book_code varchar2(20) primary key,
+ book_title varchar2(300),
+ book_author varchar2(500),
+ book_press varchar2(50),
+ book_price number
+);
+alter table book_info add (book_desc varchar2(1000));
+insert into book_info values('B0001', '더좋은삶을위한철학', '김철학','이야기',21000, '더좋은삶을위한철학 아주 재미있습니다');
+insert into book_info values('B0002', '현대철학매뉴', '박철학','뉴욕타임즈',23000, '현대철학매뉴 아주 재미있습니다');
+insert into book_info values('B0003', '호모픽투스의모험', '모닝글로리','자바출판사',15500, '호모픽투스의모험 아주 재미있습니다');
+insert into book_info values('B0004', '인류의여정', '조여정','김영사',10000, '인류의여정 아주 재미있습니다');
+insert into book_info values('B0005', '세상은어떻게돌아가는가', '저세상','그린비',22000, '세상은어떻게돌아가는가 아주 재미있습니다');
+insert into book_info values('B0006', '삶은문제해결의연속이다', '황연속','시공사',11000, '삶은문제해결의연속이다 아주 재미있습니다');
+
+insert into book_file values(1, 'B0001', '더좋은삶을위한철학.png', 'bookImages');
+insert into book_file values(2, 'B0002', '현대철학매뉴.png', 'bookImages');
+insert into book_file values(3, 'B0003', '호모픽투스의모험.png', 'bookImages');
+insert into book_file values(4, 'B0004', '인류의여정.png', 'bookImages');
+insert into book_file values(5, 'B0005', '세상은어떻게돌아가는가.png', 'bookImages');
+insert into book_file values(6, 'B0006', '삶은문제해결의연속이다.png', 'bookImages');
+
+-- book_file table.
+create table book_file (
+ file_no number primary key,
+ book_code varchar2(50),
+ book_image varchar2(50),
+ book_path varchar2(50)
+);
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+-- members table.
+create table members (
+ id varchar(10) primary key,
+ passwd varchar2(10),
+ name varchar2(30),
+ email varchar2(30),
+ responsibility varchar2(10),
+ pfileName varchar2(50),
+ ofileName varchar2(50),
+ cdate date
+);
+
+select * from members order by 1;
+
+alter table member rename column mail to email;
+alter table members add (phone_number varchar2(20));
+alter table members add (addr varchar2(100));
+
+insert into members values('user6','1234','사용자6','user6@email','User','member6.png',null,sysdate,'010-1111-2222','Daegu, jungangno-6');
+
+select * from members order by 1;
+delete from members where responsibility='User';
+update members
+set pfilename = 'admin1.png'
+where id='admin';
+
+select * from tbl_board order by 1 ;
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+-- tbl_notice table.
+create sequence notice_seq;
+create table tbl_notice (
+ notice_id number primary key,
+ notice_writer varchar2(100) not null,
+ notice_title varchar2(100) not null,
+ notice_subject varchar2(1000) not null,
+ notice_date date,
+ hit_count number default 0,
+ attach_file varchar2(100),
+ attach_dir varchar2(100)
+);
+
+select notice_seq.nextval from dual;
+insert into tbl_notice (notice_id, notice_writer, notice_title, notice_subject, notice_date)
+values(1, 'user1', 'sample title', 'sample subject', sysdate);
+
+insert into tbl_notice (notice_id, notice_writer, notice_title, notice_subject, notice_date)
+select notice_seq.nextval, 'writer'||notice_seq.currval, 'title'||notice_seq.currval, 'subject'||notice_seq.currval, sysdate
+from tbl_notice;
+
+select b.notice_id, b.notice_writer, b.notice_title, b.notice_date, b.hit_count from
+(
+    select rownum rn, a.* 
+    from (select n.*
+         from tbl_notice n
+         where 1 = 1
+         --and notice_writer like '%writer3%'
+         --and notice_title like '%title3%'
+         --and notice_subject like '%subject31%'
+         order by 1) a
+    where rownum <= (1*10)
+) b
+where b.rn >= (1*10-9);
+
+select * from tbl_notice
+where notice_id = 1;
+
+update tbl_notice
+set notice_writer = case mod(notice_id , 4) when 1 then 'user1'
+                                            when 2 then 'user2'
+                                            when 3 then 'user3'
+                                            else 'user4'
+                    end
+where notice_id > 10;
+
+select * from tbl_notice order by 1 desc;
+
+delete from tbl_notice where notice_id = 484;
+commit;
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+
+-- center_info table.
+drop table center_info purge;
+create table center_info (
+ id number,
+ center_name varchar2(100),
+ center_type varchar2(50),
+ facility_name varchar2(100),
+ org varchar2(100),
+ phone_number varchar2(20),
+ sido varchar2(40),
+ sigungu varchar2(40),
+ address varchar2(100),
+ lat varchar2(20),
+ lng varchar2(20),
+ zip_code varchar2(10),
+ created_at varchar2(30),
+ updated_at varchar2(30)
+);
+
+delete from center_info where id is not null;
+commit;
+
+select * from center_info order by 1;
+
+insert into center_info (id, center_name, center_type, facility_name, org, phone_number, sido, sigungu, address, lat, lng, zip_code, created_at, updated_at)
+values(1,'centername','centertype','facilityname','org','phone-number','sido','sigungu','address','lat','lng','zipcode','createdat','updatedat');
+
+select * from center_info
+order by id;
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------
+
+drop table genesis purge;
+create table genesis (
+ member_order varchar2(3) primary key,
+ member_name varchar2(100),
+ col1 char(1) default 'N',
+ col2 char(1) default 'N',
+ col3 char(1) default 'N',
+ col4 char(1) default 'N',
+ col5 char(1) default 'N',
+ col6 char(1) default 'N',
+ col7 char(1) default 'N',
+ col8 char(1) default 'N',
+ col9 char(1) default 'N',
+ col10 char(1) default 'N',
+ col11 char(1) default 'N',
+ col12 char(1) default 'N',
+ col13 char(1) default 'N',
+ col14 char(1) default 'N',
+ col15 char(1) default 'N'
+);
+
+select * from genesis
+order by member_order;
+
+update genesis   set col1 = 'Y'   where member_order = '1-1';
+update genesis set col2 = 'Y' where member_order = '11';
+
+---------------------------------------
+create table cart (
+	no number primary key,
+	product_nm varchar2(50),
+	price number,
+	qty number
+);
+
+insert into cart values( 1, '코드 스프링', 45000, 3);
+insert into cart values( 2, '혼자 자바', 35000, 1);
+
+commit;
