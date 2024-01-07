90468308fb580d66a6c7eac1a7b9df6c94f75f6e
you <you@email.com>
2024-01-08 08:48:50 +0900

    a

--- aws_admin.sql
+++ aws_admin.sql
@@ -1,3 +1,7 @@
+select * from all_users;
+
+alter user hr identified by hr account unlock;
+
 create user book_ex identified by book_ex;
 grant connect, resource to book_ex;
 alter user book_ex default tablespace users quota unlimited on users;
