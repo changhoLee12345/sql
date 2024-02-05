506f4f04739fe90dba4cb630bd801c87ea970309
lin <lin@home.com>
2024-02-05 12:19:19 +0900

    book_ex

--- oci_admin.sql
+++ oci_admin.sql
@@ -1,13 +1,16 @@
 
 select * from all_users order by user_id;
 
+--oracle 11g.
+alter user hr identified by hr account unlock;
+
 alter user dev identified by '';
 
 alter session set "_ORACLE_SCRIPT" = true;
 
-create user proj identified by proj;
-grant connect, resource to proj;
-alter user proj default tablespace users quota unlimited on users;
+create user book_ex identified by book_ex;
+grant connect, resource to book_ex;
+alter user book_ex default tablespace users quota unlimited on users;
 
 alter user prj identified by Qwerty123456;
 alter user dev identified by QWErty123456;
