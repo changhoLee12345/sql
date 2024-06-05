a8fd949dc2b49c4d997334cbb683e4b151b00c4d
you <you@email.com>
2024-06-05 22:20:17 +0900

    for

--- aws_admin.sql
+++ aws_admin.sql
@@ -1,3 +1,5 @@
+alter session set "_ORACLE_SCRIPT" = true;
+
 create user book_ex identified by book_ex;
 grant connect, resource to book_ex;
 alter user book_ex default tablespace users quota unlimited on users;
